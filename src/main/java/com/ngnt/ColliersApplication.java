44fc0e5647282b84483e73195a647628895397f5
Dobre Andrei <dobreandrei1@yahoo.com>
2019-12-01 12:06:36 +0200

    final version

--- src/main/java/com/ngnt/ColliersApplication.java
+++ src/main/java/com/ngnt/ColliersApplication.java
@@ -2,11 +2,13 @@ package com.ngnt;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
-import java.util.Map;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
 
@@ -15,23 +17,24 @@ import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import info.debatty.java.stringsimilarity.*;
 
 import com.ngnt.models.Output;
 import com.ngnt.models.Position;
 import com.ngnt.rest.RestClient;
 
+import info.debatty.java.stringsimilarity.Levenshtein;
+
 @SpringBootApplication
 public class ColliersApplication {
 
 	static void setColumnTitles(Levenshtein l, HashMap<Integer, String> columnMaps, int count, String cellValue) {
-		if (l.distance(cellValue.toLowerCase(), "localitate") < 1) {
+		if (l.distance(cellValue.toLowerCase(), "localitate") < 2) {
 			columnMaps.put(count, "localitate");
 		}
 		if (l.distance(cellValue.toLowerCase(), "city") < 2) {
 			columnMaps.put(count, "localitate");
 		}
-		if (l.distance(cellValue.toLowerCase(), "judet") < 2) {
+		if (l.distance(cellValue.toLowerCase(), "judet/sector") < 2) {
 			columnMaps.put(count, "judet");
 		}
 		if (l.distance(cellValue.toLowerCase(), "judet_gar") < 2) {
@@ -40,7 +43,13 @@ public class ColliersApplication {
 		if (l.distance(cellValue.toLowerCase(), "tip_str") < 2) {
 			columnMaps.put(count, "tip strada");
 		}
+		if (l.distance(cellValue.toLowerCase(), "tip artera") < 2) {
+			columnMaps.put(count, "tip strada");
+		}
 		if (l.distance(cellValue.toLowerCase(), "nume_str") < 2) {
+			columnMaps.put(count, "strada");
+		}
+		if (l.distance(cellValue.toLowerCase(), "strada") < 2) {
 			columnMaps.put(count, "strada");
 		}
 		if (l.distance(cellValue.toLowerCase(), "numar") < 2) {
@@ -49,11 +58,12 @@ public class ColliersApplication {
 		if (l.distance(cellValue.toLowerCase(), "nr_str") < 2) {
 			columnMaps.put(count, "numar");
 		}
+
 		if (l.distance(cellValue.toLowerCase(), "numar camere") < 2) {
 			columnMaps.put(count, "numar camere");
 		}
 
-		if (l.distance(cellValue.toLowerCase(), "nr_camere") < 2) {
+		if (l.distance(cellValue.toLowerCase(), "nr_camere") < 3) {
 			columnMaps.put(count, "numar camere");
 		}
 
@@ -82,7 +92,7 @@ public class ColliersApplication {
 		if (l.distance(cellValue.toLowerCase(), "an construcite") < 2) {
 			columnMaps.put(count, "an");
 		}
-		if (l.distance(cellValue.toLowerCase(), "an constr") < 2) {
+		if (l.distance(cellValue.toLowerCase(), "an constr") > 100) {
 			columnMaps.put(count, "an");
 		}
 		if (l.distance(cellValue.toLowerCase(), "finisaje") < 2) {
@@ -95,111 +105,171 @@ public class ColliersApplication {
 	}
 
 	static void constructOutput(HashMap<Integer, String> columnsMap, int count, Output output, Cell cell) {
-		if (columnsMap.get(count) == "localitate") {
-			output.setLocalitate(cell.getStringCellValue());
-		}
-		if (columnsMap.get(count) == "judet") {
-			output.setJudet(cell.getStringCellValue());
-		}
-		if (columnsMap.get(count) == "tip strada") {
-			output.setTipStrada(cell.getStringCellValue());
-		}
-		if (columnsMap.get(count) == "strada") {
-			output.setStrada(cell.getStringCellValue());
-		}
-		if (columnsMap.get(count) == "numar") {
-			output.setNumar(cell.getStringCellValue());
-		}
-		if (columnsMap.get(count) == "numar camere") {
-			output.setNumarCamere(cell.getNumericCellValue());
-		}
 
-		if (columnsMap.get(count) == "suprafata") {
-			output.setSuprafata(cell.getNumericCellValue());
-		}
+		try {
+			if (columnsMap.get(count) == "localitate") {
+				output.setLocalitate(cell.getStringCellValue());
+			}
+			if (columnsMap.get(count) == "judet") {
+				output.setJudet(cell.getStringCellValue());
+			}
 
-		if (columnsMap.get(count) == "etaj") {
-			output.setEtaj(cell.getStringCellValue());
-		}
+			if (columnsMap.get(count) == "tip strada") {
+				output.setTipStrada(cell.getStringCellValue());
+			}
+			if (columnsMap.get(count) == "strada") {
+				output.setStrada(cell.getStringCellValue());
+			}
+			if (columnsMap.get(count) == "numar") {
+				output.setNumar(cell.getStringCellValue());
+			}
+			if (columnsMap.get(count) == "numar camere") {
+				output.setNumarCamere(cell.getNumericCellValue());
+			}
 
-		if (columnsMap.get(count) == "impartire") {
-			output.setImpartire(cell.getStringCellValue());
-		}
-		if (columnsMap.get(count) == "an") {
-			output.setAnConstructie(cell.getStringCellValue());
-		}
-		if (columnsMap.get(count) == "finisaje") {
-			output.setFinisaje(cell.getStringCellValue());
-		}
-		if (columnsMap.get(count) == "id") {
-			output.setIDunic((int) cell.getNumericCellValue());
+			if (columnsMap.get(count) == "suprafata") {
+				output.setSuprafata(cell.getNumericCellValue());
+			}
+
+			if (columnsMap.get(count) == "etaj") {
+				output.setEtaj(cell.getStringCellValue());
+			}
+
+			if (columnsMap.get(count) == "impartire") {
+				output.setImpartire(cell.getStringCellValue());
+			}
+			if (columnsMap.get(count) == "an") {
+				output.setAnConstructie(cell.getStringCellValue());
+			}
+			if (columnsMap.get(count) == "finisaje") {
+				output.setFinisaje(cell.getStringCellValue());
+			}
+			if (columnsMap.get(count) == "id") {
+				output.setIDunic((int) cell.getNumericCellValue());
+			}
+		} catch (Exception e) {
+			System.err.print(e);
 		}
 
 	}
 
-	public static void main(String[] args) throws Exception, InterruptedException, ExecutionException{
+	private static void writeToCSV(List<Output> outputs, String numeFisier)
+			throws FileNotFoundException, UnsupportedEncodingException {
 
-		FileInputStream file = new FileInputStream(
-				new File("C:\\Users\\Andrei.Dobre\\Desktop\\Colliers\\1.Data\\Imobiliare.xlsx"));
+		PrintWriter writer = new PrintWriter(numeFisier, "UTF-8");
 
-		XSSFWorkbook wb = new XSSFWorkbook(file);
-		XSSFSheet sheet = wb.getSheetAt(0);
-		Iterator<Row> itr = sheet.iterator();
-
-		HashMap<Integer, String> columnsMap = new HashMap<Integer, String>();
-		boolean firstRow = true;
-		Levenshtein l = new Levenshtein();
-		List<Output> outputs = new ArrayList<Output>();
-
-		// iterating over excel file
-		while (itr.hasNext()) {
-			int count = 0;
-			Row row = itr.next();
-			Iterator<Cell> cellIterator = row.cellIterator(); // iterating over each column
-			Output output = new Output();
-			while (cellIterator.hasNext()) {
-				Cell cell = cellIterator.next();
-
-				if (firstRow) {
-					String cellValue = cell.getStringCellValue();
-					setColumnTitles(l, columnsMap, count, cellValue);
-				} else {
-					constructOutput(columnsMap, count, output, cell);
-				}
-				count++;
-			}
-			firstRow = false;
-			count = 0;
-
-			String address = output.localitate + "+" + output.judet + "+" + output.tipStrada + "+" + output.strada + "+"
-					+ output.numar;
-			String newAddress = address.replace(" ", "+");
-
-			/*
-			 * RestClient obj = new RestClient(newAddress); Future<Position> futurePosition
-			 * = obj.sendGet();
-			 * 
-			 * while (!futurePosition.isDone()) {
-			 * 
-			 * }
-			 * 
-			 * try { Position position = futurePosition.get(); System.out.println(position);
-			 * output.setLat(position.lat); output.setLng(position.lng); } catch (Exception
-			 * e) { System.err.print(e); }
-			 */
-
-			outputs.add(output);
-			//System.out.println(output);
-
-		}
-		
 		for (Output output : outputs) {
-			System.out.println(output);
+			writer.print(output.IDunic);
+			writer.print(",");
+			writer.print(output.localitate);
+			writer.print(",");
+			writer.print(output.judet);
+			writer.print(",");
+			writer.print(output.tipStrada);
+			writer.print(",");
+			writer.print(output.strada);
+			writer.print(",");
+			writer.print(output.numar);
+			writer.print(",");
+			writer.print(output.numarCamere);
+			writer.print(",");
+			writer.print(output.suprafata);
+			writer.print(",");
+			writer.print(output.etaj);
+			writer.print(",");
+			writer.print(output.impartire);
+			writer.print(",");
+			writer.print(output.anConstructie);
+			writer.print(",");
+			writer.print(output.finisaje);
+			writer.print(",");
+			writer.print(output.lat);
+			writer.print(",");
+			writer.print(output.lng);
+			writer.print("\n");
+		}
+		writer.close();
+	}
+
+	public static void main(String[] args) throws Exception, InterruptedException, ExecutionException {
+
+		for (int i = 0; i < 2; i++) {
+			FileInputStream file = new FileInputStream(
+					new File("C:\\Users\\Andrei.Dobre\\Desktop\\Colliers\\1.Data\\Imobiliare.xlsx"));
+
+			XSSFWorkbook wb = new XSSFWorkbook(file);
+			XSSFSheet sheet = wb.getSheetAt(i);
+			Iterator<Row> itr = sheet.iterator();
+
+			HashMap<Integer, String> columnsMap = new HashMap<Integer, String>();
+			boolean firstRow = true;
+			Levenshtein l = new Levenshtein();
+			List<Output> outputs = new ArrayList<Output>();
+			int max = 0;
+
+			
+			while (itr.hasNext() && max < 100) {
+				int count = 0;
+				Row row = itr.next();
+				Iterator<Cell> cellIterator = row.cellIterator(); 
+				Output output = new Output();
+				while (cellIterator.hasNext()) {
+					Cell cell = cellIterator.next();
+
+					if (firstRow) {
+						String cellValue = cell.getStringCellValue();
+						setColumnTitles(l, columnsMap, count, cellValue);
+					} else {
+						constructOutput(columnsMap, count, output, cell);
+					}
+					count++;
+				}
+				
+				firstRow = false;
+				count = 0;
+
+				if (output.localitate != null && output.judet != null && output.tipStrada != null
+						&& output.strada != null && output.numar != null) {
+					String address = output.localitate + "+" + output.judet + "+" + output.tipStrada + "+" + output.strada
+							+ "+" + output.numar;
+					String newAddress = address.replace(" ", "+");
+
+					RestClient obj = new RestClient(newAddress);
+					Future<Position> futurePosition = obj.sendGet();
+
+					while (!futurePosition.isDone()) {
+
+					}
+
+					try {
+						Position position = futurePosition.get();
+						System.out.println(position);
+						output.setLat(position.lat);
+						output.setLng(position.lng);
+					} catch (Exception e) {
+						System.err.print(e);
+					}
+				}
+
+				
+
+				if (output.suprafata != 0 && output.strada != null) {
+					outputs.add(output);
+				}
+				max ++;
+
+			}
+
+			for (Output output : outputs) {
+				System.out.println(output);
+			}
+
+			writeToCSV(outputs, "outputs" + i + ".txt");
+
+			wb.close();
+
 		}
 
-		wb.close();
-
-		// SpringApplication.run(ColliersApplication.class, args);
 	}
 
 }
